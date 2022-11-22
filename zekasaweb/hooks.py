3784f80d55f3cfd7bb2fb2ffdb8492d5e56b440e
finbyz <info@finbyz.tech>
2022-11-22 18:12:13 +0530

    base file chnage for version 14

--- zekasaweb/hooks.py
+++ zekasaweb/hooks.py
@@ -1,4 +1,4 @@
-from . import __version__ as app_version
+# from . import __version__ as app_version
 
 app_name = "zekasaweb"
 app_title = "Zekasaweb"
@@ -10,171 +10,171 @@ app_email = "info@finbyz.com"
 app_license = "GPL 3.0"
 app_logo_url = "/files/zekasa-black-logo.png"
 
-# Includes in <head>
-# ------------------
+# # Includes in <head>
+# # ------------------
 
-# include js, css files in header of desk.html
-# app_include_css = "/assets/zekasaweb/css/zekasaweb.css"
-# app_include_js = "/assets/zekasaweb/js/zekasaweb.js"
+# # include js, css files in header of desk.html
+# # app_include_css = "/assets/zekasaweb/css/zekasaweb.css"
+# # app_include_js = "/assets/zekasaweb/js/zekasaweb.js"
 
-# include js, css files in header of web template
-# web_include_css = "/assets/zekasaweb/css/zekasaweb.css"
-# web_include_js = "/assets/zekasaweb/js/zekasaweb.js"
+# # include js, css files in header of web template
+# # web_include_css = "/assets/zekasaweb/css/zekasaweb.css"
+# # web_include_js = "/assets/zekasaweb/js/zekasaweb.js"
 
-# include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "zekasaweb/public/scss/website"
+# # include custom scss in every website theme (without file extension ".scss")
+# # website_theme_scss = "zekasaweb/public/scss/website"
 
-# include js, css files in header of web form
-# webform_include_js = {"doctype": "public/js/doctype.js"}
-# webform_include_css = {"doctype": "public/css/doctype.css"}
+# # include js, css files in header of web form
+# # webform_include_js = {"doctype": "public/js/doctype.js"}
+# # webform_include_css = {"doctype": "public/css/doctype.css"}
 
-# include js in page
-# page_js = {"page" : "public/js/file.js"}
+# # include js in page
+# # page_js = {"page" : "public/js/file.js"}
 
-# include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+# # include js in doctype views
+# # doctype_js = {"doctype" : "public/js/doctype.js"}
+# # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
-# Home Pages
-# ----------
+# # Home Pages
+# # ----------
 
-# application home page (will override Website Settings)
-# home_page = "login"
+# # application home page (will override Website Settings)
+# # home_page = "login"
 
-# website user home page (by Role)
-# role_home_page = {
-#	"Role": "home_page"
-# }
+# # website user home page (by Role)
+# # role_home_page = {
+# #	"Role": "home_page"
+# # }
 
-# Generators
-# ----------
+# # Generators
+# # ----------
 
-# automatically create page for each record of this doctype
-# website_generators = ["Web Page"]
+# # automatically create page for each record of this doctype
+# # website_generators = ["Web Page"]
 
-# Installation
-# ------------
+# # Installation
+# # ------------
 
-# before_install = "zekasaweb.install.before_install"
-# after_install = "zekasaweb.install.after_install"
+# # before_install = "zekasaweb.install.before_install"
+# # after_install = "zekasaweb.install.after_install"
 
-# Desk Notifications
-# ------------------
-# See frappe.core.notifications.get_notification_config
+# # Desk Notifications
+# # ------------------
+# # See frappe.core.notifications.get_notification_config
 
-# notification_config = "zekasaweb.notifications.get_notification_config"
+# # notification_config = "zekasaweb.notifications.get_notification_config"
 
-# Permissions
-# -----------
-# Permissions evaluated in scripted ways
+# # Permissions
+# # -----------
+# # Permissions evaluated in scripted ways
 
-# permission_query_conditions = {
-# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
-# }
-#
-# has_permission = {
-# 	"Event": "frappe.desk.doctype.event.event.has_permission",
-# }
+# # permission_query_conditions = {
+# # 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# # }
+# #
+# # has_permission = {
+# # 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# # }
 
-# DocType Class
-# ---------------
-# Override standard doctype classes
+# # DocType Class
+# # ---------------
+# # Override standard doctype classes
 
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+# # override_doctype_class = {
+# # 	"ToDo": "custom_app.overrides.CustomToDo"
+# # }
 
-# Document Events
-# ---------------
-# Hook on document methods and events
+# # Document Events
+# # ---------------
+# # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-#	}
-# }
+# # doc_events = {
+# # 	"*": {
+# # 		"on_update": "method",
+# # 		"on_cancel": "method",
+# # 		"on_trash": "method"
+# #	}
+# # }
 
-# Scheduled Tasks
-# ---------------
+# # Scheduled Tasks
+# # ---------------
 
-# scheduler_events = {
-# 	"all": [
-# 		"zekasaweb.tasks.all"
-# 	],
-# 	"daily": [
-# 		"zekasaweb.tasks.daily"
-# 	],
-# 	"hourly": [
-# 		"zekasaweb.tasks.hourly"
-# 	],
-# 	"weekly": [
-# 		"zekasaweb.tasks.weekly"
-# 	]
-# 	"monthly": [
-# 		"zekasaweb.tasks.monthly"
-# 	]
-# }
+# # scheduler_events = {
+# # 	"all": [
+# # 		"zekasaweb.tasks.all"
+# # 	],
+# # 	"daily": [
+# # 		"zekasaweb.tasks.daily"
+# # 	],
+# # 	"hourly": [
+# # 		"zekasaweb.tasks.hourly"
+# # 	],
+# # 	"weekly": [
+# # 		"zekasaweb.tasks.weekly"
+# # 	]
+# # 	"monthly": [
+# # 		"zekasaweb.tasks.monthly"
+# # 	]
+# # }
 
-# Testing
-# -------
+# # Testing
+# # -------
 
-# before_tests = "zekasaweb.install.before_tests"
+# # before_tests = "zekasaweb.install.before_tests"
 
-# Overriding Methods
-# ------------------------------
-#
-# override_whitelisted_methods = {
-# 	"frappe.desk.doctype.event.event.get_events": "zekasaweb.event.get_events"
-# }
-#
-# each overriding function accepts a `data` argument;
-# generated from the base implementation of the doctype dashboard,
-# along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-# 	"Task": "zekasaweb.task.get_dashboard_data"
-# }
+# # Overriding Methods
+# # ------------------------------
+# #
+# # override_whitelisted_methods = {
+# # 	"frappe.desk.doctype.event.event.get_events": "zekasaweb.event.get_events"
+# # }
+# #
+# # each overriding function accepts a `data` argument;
+# # generated from the base implementation of the doctype dashboard,
+# # along with any modifications made in other Frappe apps
+# # override_doctype_dashboards = {
+# # 	"Task": "zekasaweb.task.get_dashboard_data"
+# # }
 
-# exempt linked doctypes from being automatically cancelled
-#
-# auto_cancel_exempted_doctypes = ["Auto Repeat"]
-
-
-# User Data Protection
-# --------------------
-
-user_data_fields = [
-	{
-		"doctype": "{doctype_1}",
-		"filter_by": "{filter_by}",
-		"redact_fields": ["{field_1}", "{field_2}"],
-		"partial": 1,
-	},
-	{
-		"doctype": "{doctype_2}",
-		"filter_by": "{filter_by}",
-		"partial": 1,
-	},
-	{
-		"doctype": "{doctype_3}",
-		"strict": False,
-	},
-	{
-		"doctype": "{doctype_4}"
-	}
-]
-web_include_js = [
-	"/assets/js/zekasa-product-listening.min.js",
-]
+# # exempt linked doctypes from being automatically cancelled
+# #
+# # auto_cancel_exempted_doctypes = ["Auto Repeat"]
 
 
-# Authentication and authorization
-# --------------------------------
+# # User Data Protection
+# # --------------------
 
-# auth_hooks = [
-# 	"zekasaweb.auth.validate"
+# user_data_fields = [
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
 # ]
+# # web_include_js = [
+# # 	"/assets/js/zekasa-product-listening.min.js",
+# # ]
+
+
+# # Authentication and authorization
+# # --------------------------------
+
+# # auth_hooks = [
+# # 	"zekasaweb.auth.validate"
+# # ]
 
